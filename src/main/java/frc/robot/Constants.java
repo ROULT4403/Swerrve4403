8a7ccce979c387fece35842338d414970c50ea15
Tejada_06 <sebastian.tejadam2@gmail.com>
2023-11-04 11:11:15 -0600

    New Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -21,7 +22,7 @@ public final class Constants
   {
     public static final int[] driveMotorIDs = new int[] {10, 3, 1, 4};
     public static final int[] turningMotorIDs = new int[] {5, 7, 6, 8};
-    public static final int[] absoluteEncodersChannels = new int[] {0, 0, 0, 0};
+    public static final int[] absoluteEncodersChannels = new int[] {5, 7, 6, 8};
     public static final boolean[] driveInverted = new boolean[] {false, true, true, true};
     public static final boolean[] turningInverted = new boolean[] {false, true, false, false};
     
@@ -30,12 +31,12 @@ public final class Constants
     public static final double[] driveP = new double[] {1, 1, 1, 1};
     public static final double[] driveI = new double[] {0, 0, 0, 0};
 
-    public static final double[] turnP = new double[] {1, 1, 1, 1};
+    public static final double[] turnP = new double[] {1, 0.001, 1, 0.001};
     public static final double[] turnI = new double[] {0, 0, 0, 0};
 
-    public static final double kTrackWidth = 0.5;
+    public static final double kTrackWidth = Units.inchesToMeters(19);
 
-    public static final double kWheelBase = 0.7;
+    public static final double kWheelBase = Units.inchesToMeters(19);
 
     public static final SwerveDriveKinematics kinematics =
         new SwerveDriveKinematics(
@@ -54,8 +55,8 @@ public final class Constants
     public static final double turningSpeed = 2 * Math.PI;
     public static final double turningAcceleration = 2 * Math.PI;
 
-    public static final int kEncoderCPR = 1024;
-    public static final double kWheelDiameterMeters = 0.15;
+    public static final int kEncoderCPR = 42 * (27/4);
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
     public static final double kDriveEncoderDistancePerPulse =
         // Assumes the encoders are directly mounted on the wheel shafts
         (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
